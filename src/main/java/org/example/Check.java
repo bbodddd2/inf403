aa98ea31e2d04aadb3080868ffb1e5727ff63702
Bogdan Yakovlev <bogdanyakovlev539@gmail.com>
2025-05-04 00:08:11 +0300

    bigbob

--- src/main/java/org/example/Check.java
+++ src/main/java/org/example/Check.java
@@ -1,4 +1,47 @@
 package org.example;
 
-public class  Check {
+
+
+import java.util.Stack;
+
+public class Check {
+    public static void main(String[] args) {
+        String[] str = {"()", "}", "{)}", "}", ")", "[", "]"};
+        for (String s : str) {
+            boolean verification = check(s);
+            System.out.println(verification);
+        }
+
+
+    }
+
+    public static boolean isPair(char bracket, char turnBracket) {
+        return (bracket == '(' && turnBracket == ')') || (bracket == '{' && turnBracket == '}') ||
+                (bracket == '[' && turnBracket == ']');
+    }
+
+    public static boolean check(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char last = st.pop();
+                if (!isPair(last, ch)) {
+                    return false;
+                }
+
+            }
+
+
+        }
+        return st.isEmpty();
+
+
+    }
+
 }
